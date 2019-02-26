bc3864336abcdd6c225c7a23be7639e1954314bb
孔东 <kongdong@repeatlink.cn>
2019-02-26 11:49:57 +0800

    新增底层库

--- android/src/main/java/com/lib/RNLibModule.java
+++ android/src/main/java/com/lib/RNLibModule.java
@@ -1,10 +1,13 @@
 
 package com.lib;
 
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.WritableMap;
+import com.lib.utils.Device;
 
 public class RNLibModule extends ReactContextBaseJavaModule {
 
@@ -18,5 +21,21 @@ public class RNLibModule extends ReactContextBaseJavaModule {
   @Override
   public String getName() {
     return "RNLib";
+  }
+
+  /**
+   * 拍照方法
+   * @param callback
+   */
+  @ReactMethod
+  public void takePhoto(Callback callback){
+
+  }
+  @ReactMethod
+  public void getAppInfo(Callback callback){
+    WritableMap map = Arguments.createMap();
+    map.putInt("versionCode", Device.getVersionCode(reactContext));
+    map.putString("versionName", Device.getVersionName(reactContext));
+    callback.invoke(map);
   }
 }
---
+++ android/src/main/java/com/lib/utils/Device.java
@@ -0,0 +1,30 @@
+package com.lib.utils;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+
+/**
+ * Created by Administrator on 2019/2/26.
+ */
+
+public class Device {
+    public static int getVersionCode(Context mContext) {
+        if (mContext != null) {
+            try {
+                return mContext.getPackageManager().getPackageInfo(mContext.getPackageName(), 0).versionCode;
+            } catch (PackageManager.NameNotFoundException ignored) {
+            }
+        }
+        return 0;
+    }
+
+    public static String getVersionName(Context mContext) {
+        if (mContext != null) {
+            try {
+                return mContext.getPackageManager().getPackageInfo(mContext.getPackageName(), 0).versionName;
+            } catch (PackageManager.NameNotFoundException ignored) {
+            }
+        }
+        return "";
+    }
+}
